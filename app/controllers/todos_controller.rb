c04c723ef67fd7499574dd3a70bb37a7d429e8dd
vaitheeswarivenkatesan <56390406+vaitheeswarivenkatesan@users.noreply.github.com>
2021-05-22 03:33:25 +0530

    Update todos_controller.rb

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,29 +1,31 @@
-class TodosController < ApplicationController
+class UsersController < ApplicationController
   skip_before_action :verify_authenticity_token
 
+  # To show all the users
   def index
-    render plain: Todo.order(:due_date).map { |todo| todo.to_pleasant_string }.join("\n")
+    render plain: User.all.map { |user| user.to_pleasant_user_string }.join("\n")
   end
 
+  # To create a new user
   def create
-    todo_text = params[:todo_text]
-    due_date = DateTime.parse(params[:due_date])
-    new_todo = Todo.create!(todo_text: todo_text, due_date: due_date, completed: false)
-    response_text = "New todo is created with id #{new_todo.id}"
+    name = params[:name]
+    email = params[:email]
+    password = params[:password]
+    new_user = User.create!(name: name, email: email, password: password)
+    response_text = "New User is created with id #{new_user.id}"
     render plain: response_text
   end
 
-  def update
-    id = params[:id]
-    completed = params[:completed]
-    todo = Todo.find(id)
-    todo.completed = completed
-    todo.save
-    render plain: "Updated Todo  #{todo.id} with completed status : #{todo.completed}"
-  end
-
-  def show
-    id = params[:id]
-    render plain: Todo.find(id).to_pleasant_string
+  # To check if there is a user with the given email and password
+  def login
+    email = params[:email]
+    password = params[:password]
+    user = User.find { |user| user.email == email }
+    if !(user == nil)
+      response_text = (user[:password].eql? password) ? "true" : "false"
+    else
+      response_text = "false"
+    end
+    render plain: response_text
   end
 end
