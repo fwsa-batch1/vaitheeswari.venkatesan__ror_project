74b21a1aea7493694276938f76df6e8d9ed82314
vaitheeswarivenkatesan <v.vaithees4@gmail.com>
2021-05-29 12:42:02 +0530

    Add authenticity token

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,5 @@
 class TodosController < ApplicationController
-  skip_before_action :verify_authenticity_token
+  # skip_before_action :verify_authenticity_token
 
   # To show all the todos
   def index
