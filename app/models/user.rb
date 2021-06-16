45fdb550034fca166d47df869b3bc626ce0d2f9b
vaitheeswarivenkatesan <v.vaithees4@gmail.com>
2021-06-16 13:12:03 +0530

    Changed User.rb email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   validates :first_name, presence: true
-  validates :email, presence: true
+  validates :email, presence: true, uniqueness: true
   has_many :todos
   has_secure_password
 end
